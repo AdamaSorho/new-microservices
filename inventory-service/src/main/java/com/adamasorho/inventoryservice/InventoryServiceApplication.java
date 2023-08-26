e6b1730dd6db95b953f67a13e1d85ac49154aa4b
AdamaSorho <djomantcha@gmail.com>
2023-08-26 18:54:49 -0500

    Update Inventory and Order Services database from Mysql to PostgreSql

--- inventory-service/src/main/java/com/adamasorho/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/adamasorho/inventoryservice/InventoryServiceApplication.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class InventoryServiceApplication implements CommandLineRunner {
+public class InventoryServiceApplication {
 	@Autowired
 	private InventoryRepository inventoryRepository;
 
@@ -18,18 +19,20 @@ public class InventoryServiceApplication implements CommandLineRunner {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Inventory inventory1 = Inventory.builder()
-				.skuCode("Iphone_13")
-				.quantity(100)
-				.build();
-		Inventory inventory2 = Inventory.builder()
-				.skuCode("Iphone_12")
-				.quantity(0)
-				.build();
+	@Bean
+	public CommandLineRunner CommandLineRunnerBean() {
+		return (args) -> {
+			Inventory inventory1 = Inventory.builder()
+					.skuCode("Iphone_13")
+					.quantity(100)
+					.build();
+			Inventory inventory2 = Inventory.builder()
+					.skuCode("Iphone_12")
+					.quantity(0)
+					.build();
 
-		inventoryRepository.save(inventory1);
-		inventoryRepository.save(inventory2);
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
 	}
 }
